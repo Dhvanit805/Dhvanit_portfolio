cdc2462cc19fcc7ef08cfc514a9e5eb0864f2e25
Dhvanit137 <2101031000137@silveroakuni.ac.in>
2025-02-08 12:48:51 +0530

    change in home page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Dhvanit_portfolio
+git config --global user.name "Dhvanit137"
+git config --global user.email "2101031000137@silveroakuni.ac.in"
+git config --global credential.helper cache
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -46,57 +46,6 @@ function Home2() {
             </Tilt>
           </Col>
         </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me!
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
-                  href="https://github.com/Dhvanit137"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://twitter.com/your-username"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li> */}
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/dhvanitpatel0805?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                  
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/dhwanit805?igsh=MXA1bWVkZjQ2dm1kYg=="
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
       </Container>
     </Container>
   );
